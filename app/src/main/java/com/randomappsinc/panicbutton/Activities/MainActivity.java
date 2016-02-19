3c5e1ee0e2aaa9c1cbafd13decdfef85a4f8a32f
Gear61 <chessnone@yahoo.com>
2016-02-18 18:59:20 -0800

    Deal with send SMS and access location

--- app/src/main/java/com/randomappsinc/panicbutton/Activities/MainActivity.java
+++ app/src/main/java/com/randomappsinc/panicbutton/Activities/MainActivity.java
@@ -1,15 +1,23 @@
 package com.randomappsinc.panicbutton.Activities;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.joanzapata.iconify.IconDrawable;
 import com.joanzapata.iconify.fonts.FontAwesomeIcons;
 import com.randomappsinc.panicbutton.R;
+import com.randomappsinc.panicbutton.Utils.PermissionUtils;
 import com.randomappsinc.panicbutton.Utils.PreferencesManager;
 import com.randomappsinc.panicbutton.Utils.UIUtils;
 
@@ -18,20 +26,17 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends SlidingActivity {
-    private static final int SEND_SMS_REQUEST = 1;
-    private static final int ACCESS_LOCATION_REQUEST = 2;
+    private static final int SEND_SMS_CODE = 1;
+    private static final int ACCESS_LOCATION_CODE = 2;
 
     @Bind(R.id.panic_button) FloatingActionButton panicButton;
     @Bind(R.id.panic_instructions) TextView instructions;
-
-    private boolean firstTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         if (PreferencesManager.get().getEmergencyContacts().isEmpty()) {
-            firstTime = true;
             startActivity(new Intent(this, ChooseContactsActivity.class));
         }
 
@@ -46,8 +51,32 @@ public class MainActivity extends SlidingActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if (firstTime && !PreferencesManager.get().getEmergencyContacts().isEmpty()) {
+        if (!PreferencesManager.get().getEmergencyContacts().isEmpty()) {
+            if (!PermissionUtils.isPermissionGranted(Manifest.permission.SEND_SMS)) {
+                getPermission(Manifest.permission.SEND_SMS, R.string.send_sms_explanation, SEND_SMS_CODE);
+            }
+            else if (!PermissionUtils.isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                getPermission(Manifest.permission.ACCESS_FINE_LOCATION, R.string.location_explanation, ACCESS_LOCATION_CODE);
+            }
+        }
+    }
 
+    private void getPermission(final String permission, int explanationId, final int requestCode) {
+        final Activity activity = this;
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+            new MaterialDialog.Builder(this)
+                    .content(explanationId)
+                    .positiveText(android.R.string.yes)
+                    .onPositive(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                            PermissionUtils.requestPermission(activity, permission, requestCode);
+                        }
+                    })
+                    .show();
+        }
+        else {
+            PermissionUtils.requestPermission(this, permission, requestCode);
         }
     }
 
